c3d82d60040458db5cc56fbba1dbb116ded3b631
Niko Neufeld <niko@nikos-macbook.local>
2016-09-11 08:05:49 +0200

    Initial Commit

---
+++ ninemensmorris/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ninemensmorris
+//
+//  Created by Niko Neufeld on 9/11/16.
+//  Copyright © 2016 pinbeutel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
